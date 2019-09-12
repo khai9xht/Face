2d4b4b00d5df0a3880cdb82323bd51276679f36d
khai9xht <khaihoang9x@gmail.com>
2019-09-12 14:46:52 +0700

    edit folder view and public

--- public/js/faceDetectionControls.js
+++ public/js/faceDetectionControls.js
@@ -1,7 +1,4 @@
-const SSD_MOBILENETV1 = 'ssd_mobilenetv1'
 const TINY_FACE_DETECTOR = 'tiny_face_detector'
-const MTCNN = 'mtcnn'
-
 
 let selectedFaceDetector = TINY_FACE_DETECTOR
 
@@ -14,40 +11,6 @@ function getFaceDetectorOptions() {
   return new faceapi.TinyFaceDetectorOptions({ inputSize, scoreThreshold });
 }
 
-function onIncreaseMinConfidence() {
-  minConfidence = Math.min(faceapi.round(minConfidence + 0.1), 1.0)
-  $('#minConfidence').val(minConfidence)
-  updateResults()
-}
-
-function onDecreaseMinConfidence() {
-  minConfidence = Math.max(faceapi.round(minConfidence - 0.1), 0.1)
-  $('#minConfidence').val(minConfidence)
-  updateResults()
-}
-
-function onIncreaseScoreThreshold() {
-  scoreThreshold = Math.min(faceapi.round(scoreThreshold + 0.1), 1.0)
-  $('#scoreThreshold').val(scoreThreshold)
-  updateResults()
-}
-
-function onDecreaseScoreThreshold() {
-  scoreThreshold = Math.max(faceapi.round(scoreThreshold - 0.1), 0.1)
-  $('#scoreThreshold').val(scoreThreshold)
-  updateResults()
-}
-
-function onIncreaseMinFaceSize() {
-  minFaceSize = Math.min(faceapi.round(minFaceSize + 20), 300)
-  $('#minFaceSize').val(minFaceSize)
-}
-
-function onDecreaseMinFaceSize() {
-  minFaceSize = Math.max(faceapi.round(minFaceSize - 20), 50)
-  $('#minFaceSize').val(minFaceSize)
-}
-
 function getCurrentFaceDetectionNet() {
   return faceapi.nets.tinyFaceDetector
 }
@@ -57,13 +20,6 @@ function isFaceDetectionModelLoaded() {
 }
 
 async function changeFaceDetector(detector) {
-  ['#ssd_mobilenetv1_controls', '#tiny_face_detector_controls', '#mtcnn_controls']
-    .forEach(id => $(id).hide())
-
-  selectedFaceDetector = detector
-  const faceDetectorSelect = $('#selectFaceDetector')
-  faceDetectorSelect.val(detector)
-  faceDetectorSelect.material_select()
 
   $('#loader').show()
   if (!isFaceDetectionModelLoaded()) {
@@ -72,18 +28,4 @@ async function changeFaceDetector(detector) {
 
   $(`#${detector}_controls`).show()
   $('#loader').hide()
-}
-
-async function onSelectedFaceDetectorChanged(e) {
-  selectedFaceDetector = e.target.value
-
-  await changeFaceDetector(e.target.value)
-  updateResults()
-}
-
-function initFaceDetectionControls() {
-  const faceDetectorSelect = $('#selectFaceDetector')
-  faceDetectorSelect.val(selectedFaceDetector)
-  faceDetectorSelect.on('change', onSelectedFaceDetectorChanged)
-  faceDetectorSelect.material_select()
 }
